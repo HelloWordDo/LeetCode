b0875987bf91abc61e9c2f26f594f2fed93136b5
liquancheng <quancheng.li@mobilityasia.com.cn>
2020-04-20 11:45:59 +0800

    1025. 除数博弈

---
+++ src/main/java/com/leetcode/dynamic/programming/DivisorGame_1025.java
@@ -0,0 +1,31 @@
+package com.leetcode.dynamic.programming;
+
+/**
+ * 1025. 除数博弈
+ */
+public class DivisorGame_1025 {
+
+    public static boolean divisorGame(int N) {
+
+        if (N == 1) return false;
+        if (N == 2) return true;
+        boolean[] dp = new boolean[N + 1];
+        dp[1] = false;
+        dp[2] = true;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = false;
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0 && !dp[i - j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[N];
+    }
+
+    public static void main(String[] args) {
+        System.out.println("结果：" + divisorGame(10));
+    }
+}
