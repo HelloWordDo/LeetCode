ee99f2629f049728640ac9c7b8cd89f6e95ca5ab
liquancheng <quancheng.li@mobilityasia.com.cn>
2020-04-22 17:58:49 +0800

    139. 单词拆分

---
+++ src/main/java/com/leetcode/dynamic/programming/WordBreak_139.java
@@ -0,0 +1,38 @@
+package com.leetcode.dynamic.programming;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 139. 单词拆分
+ */
+public class WordBreak_139 {
+
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        List<String> wordDict = new ArrayList<>();
+        wordDict.add("cats");
+        wordDict.add("dog");
+        wordDict.add("sand");
+        wordDict.add("and");
+        wordDict.add("cat");
+        System.out.println("结果：" + wordBreak("catsandog", wordDict));
+    }
+}
