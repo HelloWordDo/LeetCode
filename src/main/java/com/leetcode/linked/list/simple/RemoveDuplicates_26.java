a02c334dba6ac30d1a453c7ae48b9083ba89a758
liquancheng <quancheng.li@mobilityasia.com.cn>
2021-01-28 16:50:15 +0800

    26. 删除排序数组中的重复项

---
+++ src/main/java/com/leetcode/linked/list/simple/RemoveDuplicates_26.java
@@ -0,0 +1,42 @@
+package com.leetcode.linked.list.simple;
+
+/**
+ *
+ */
+public class RemoveDuplicates_26 {
+
+    public static int removeDuplicates(int[] nums) {
+
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return 1;
+        }
+        int index = 1;
+
+        int i = 0;
+        int j = 1;
+        while (i < nums.length - 1) {
+
+            if (j >= nums.length) {
+                break;
+            }
+
+            if (nums[i] == nums[j]) {
+                j++;
+                continue;
+            }
+
+            i = j;
+            nums[index] = nums[j];
+            index++;
+            j++;
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(new int[]{0, 0}));
+    }
+}
