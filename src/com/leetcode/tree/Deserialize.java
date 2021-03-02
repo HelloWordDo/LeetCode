2ed23b9ce309917b5452ec9674b257f4dd6ba005
liquancheng <quancheng.li@mobilityasia.com.cn>
2021-03-02 11:55:50 +0800

    二叉树反序列化

---
+++ src/com/leetcode/tree/Deserialize.java
@@ -0,0 +1,53 @@
+package com.leetcode.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 二叉树反序列化
+ */
+public class Deserialize {
+
+    public static String SEP = ",";
+    public static String NULL = "#";
+
+    /**
+     * 层序遍历
+     * 将字符串反序列化为二叉树结构
+     *
+     * @param data
+     * @return
+     */
+    public static TreeNode LevelOrderDeserialize(String data) {
+        if (data.isEmpty()) return null;
+        String[] nodes = data.split(SEP);
+        // 第一个元素就是 root 的值
+        TreeNode root = new TreeNode(Integer.parseInt(nodes[0]));
+
+        // 队列 q 记录父节点，将 root 加入队列
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        for (int i = 1; i < nodes.length; ) {
+            // 队列中存的都是父节点
+            TreeNode parent = q.poll();
+            // 父节点对应的左侧子节点的值
+            String left = nodes[i++];
+            if (!left.equals(NULL)) {
+                parent.left = new TreeNode(Integer.parseInt(left));
+                q.offer(parent.left);
+            } else {
+                parent.left = null;
+            }
+            // 父节点对应的右侧子节点的值
+            String right = nodes[i++];
+            if (!right.equals(NULL)) {
+                parent.right = new TreeNode(Integer.parseInt(right));
+                q.offer(parent.right);
+            } else {
+                parent.right = null;
+            }
+        }
+        return root;
+    }
+}
