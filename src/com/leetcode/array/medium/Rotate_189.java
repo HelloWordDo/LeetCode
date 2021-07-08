07e51f36ccb3c13c139ad147cb2cb86176902266
liquancheng <quancheng.li@mobilityasia.com.cn>
2021-07-08 15:37:43 +0800

    189. 旋转数组

---
+++ src/com/leetcode/array/medium/Rotate_189.java
@@ -0,0 +1,63 @@
+package com.leetcode.array.medium;
+
+/**
+ * 189. 旋转数组
+ * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ * <p>
+ * <p>
+ * 进阶：
+ * 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+ * 你可以使用空间复杂度为 O(1) 的 原地 算法解决这个问题吗？
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [1,2,3,4,5,6,7], k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右旋转 1 步: [7,1,2,3,4,5,6]
+ * 向右旋转 2 步: [6,7,1,2,3,4,5]
+ * 向右旋转 3 步: [5,6,7,1,2,3,4]
+ * 示例 2:
+ * <p>
+ * 输入：nums = [-1,-100,3,99], k = 2
+ * 输出：[3,99,-1,-100]
+ * 解释:
+ * 向右旋转 1 步: [99,-1,-100,3]
+ * 向右旋转 2 步: [3,99,-1,-100]
+ */
+public class Rotate_189 {
+
+    public void rotate(int[] nums, int k) {
+
+        if (k >= nums.length) {
+            k = k % nums.length;
+        }
+
+        reverse(nums, 0, nums.length - 1);
+
+        reverse(nums, 0, k - 1);
+
+        reverse(nums, k, nums.length - 1);
+
+        System.out.println(nums);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[end];
+            nums[end] = nums[start];
+
+            nums[start] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Rotate_189 method = new Rotate_189();
+
+        method.rotate(new int[]{1, 2, 3}, 1);
+
+    }
+}
