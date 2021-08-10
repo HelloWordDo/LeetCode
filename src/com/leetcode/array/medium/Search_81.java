e7e8b6147d71981114ac541ae96f97b6ee366052
liquancheng <quancheng.li@mobilityasia.com.cn>
2021-08-10 10:56:19 +0800

    81. 搜索旋转排序数组 II

---
+++ src/com/leetcode/array/medium/Search_81.java
@@ -0,0 +1,84 @@
+package com.leetcode.array.medium;
+
+/**
+ * 81. 搜索旋转排序数组 II
+ * 已知存在一个按非降序排列的整数数组 nums ，数组中的值不必互不相同。
+ * <p>
+ * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转 ，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,4,4,5,6,6,7] 在下标 5 处经旋转后可能变为 [4,5,6,6,7,0,1,2,4,4] 。
+ * <p>
+ * 给你 旋转后 的数组 nums 和一个整数 target ，请你编写一个函数来判断给定的目标值是否存在于数组中。如果 nums 中存在这个目标值 target ，则返回 true ，否则返回 false 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,5,6,0,0,1,2], target = 0
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：nums = [2,5,6,0,0,1,2], target = 3
+ * 输出：false
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 5000
+ * -104 <= nums[i] <= 104
+ * 题目数据保证 nums 在预先未知的某个下标上进行了旋转
+ * -104 <= target <= 104
+ * <p>
+ * <p>
+ * 进阶：
+ * <p>
+ * 这是 搜索旋转排序数组 的延伸题目，本题中的 nums  可能包含重复元素。
+ * 这会影响到程序的时间复杂度吗？会有怎样的影响，为什么？
+ */
+public class Search_81 {
+
+    public boolean search(int[] nums, int target) {
+
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+
+            while (left < right && nums[left] == nums[left + 1]) {
+                left++;
+            }
+            while (left < right && nums[right] == nums[right - 1]) {
+                right--;
+            }
+
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            //右有序
+            if (nums[right] > nums[mid]) {
+
+                if (nums[mid] < target && nums[right] >= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+
+            } else {
+                //左有序
+
+                if (nums[mid] > target && nums[left] <= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+
+        Search_81 method = new Search_81();
+        System.out.println(method.search(new int[]{2, 5, 6, 0, 0, 1, 2}, 3));
+    }
+}
