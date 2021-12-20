ac0287e6fe890afc8652c4b8196f381936b7627f
liquancheng <quancheng.li@mobilityasia.com.cn>
2021-12-20 18:06:11 +0800

    31. 下一个排列

---
+++ src/com/leetcode/array/medium/Next_Permutation_31.java
@@ -0,0 +1,87 @@
+package com.leetcode.array.medium;
+
+/**
+ * 31. 下一个排列
+ * 实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列（即，组合出下一个更大的整数）。
+ * <p>
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ * <p>
+ * 必须 原地 修改，只允许使用额外常数空间。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,2,3]
+ * 输出：[1,3,2]
+ * 示例 2：
+ * <p>
+ * 输入：nums = [3,2,1]
+ * 输出：[1,2,3]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [1,1,5]
+ * 输出：[1,5,1]
+ * 示例 4：
+ * <p>
+ * 输入：nums = [1]
+ * 输出：[1]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 100
+ */
+public class Next_Permutation_31 {
+
+    public void nextPermutation(int[] nums) {
+
+        int j = nums.length - 1;
+        int i = j - 1;
+
+        while (i >= 0 && nums[i] >= nums[j]) {
+            j--;
+            i--;
+        }
+
+        if (i >= 0) {
+            for (int k = nums.length - 1; k > j; k--) {
+                if (nums[k] > nums[i]) {
+                    j = k;
+                    break;
+                }
+            }
+
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        int left = i + 1;
+        int right = nums.length - 1;
+
+        int mid = (nums.length + left) / 2;
+
+        while (left < mid) {
+            int tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+            left++;
+            right--;
+        }
+
+        System.out.println(nums);
+    }
+
+    public static void main(String[] args) {
+
+        Next_Permutation_31 method = new Next_Permutation_31();
+//        method.nextPermutation(new int[]{1, 2, 3});
+//        method.nextPermutation(new int[]{1, 3, 2});
+//        method.nextPermutation(new int[]{3, 2, 1, 0});
+//        method.nextPermutation(new int[]{3, 2, 1});
+        method.nextPermutation(new int[]{2, 2, 7, 5, 4, 3, 2, 2, 1});
+        method.nextPermutation(new int[]{5, 1, 1});
+
+    }
+}
